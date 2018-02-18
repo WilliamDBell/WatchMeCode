36f738331a4934aa3cb9a42e12abf4f7da131b95
William Bell <williambell@William.local>
2018-02-18 16:29:27 -0500

    initial commit

---
+++ robot.py
@@ -0,0 +1,63 @@
+# Name:
+
+# See the README file for instructions.
+
+from main import TurnOn
+from main import TurnOff
+from main import TurnLeft
+from main import Move
+from main import PickUp
+from main import PutDown
+
+from main import isAtPickUp
+from main import isAtDeposit
+from main import isAtEnd
+from main import isClearAhead
+from main import isClearLeft
+from main import isClearRight
+from main import hasPickUps
+from main import hasError
+
+
+from glob import UP
+from glob import RIGHT
+from glob import LEFT
+from glob import DOWN
+
+# This can be any value 0-7.  Chane it to test each function.
+CURRENT_GAME = 0
+
+# Set this to False to just test all your results without a GUI.  When set to True
+# it will test one puzzle at a time.
+SHOW_GUI = True
+
+# The number of miliseconds between each step.  Raise it to slow things down.
+# Lower it to speed it up.
+STEP_TIME = 500
+
+
+
+def solve0():
+  pass # pass does nothing. delete it and add your own code.
+
+def solve1():
+  pass
+
+def solve2():
+  pass
+
+def solve3():
+  pass
+
+def solve4():
+  pass
+
+def solve5():
+  pass
+
+def solve6():
+  pass
+
+def solve7():
+  pass
+
